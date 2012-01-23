302133eaab16c9caa9f89df5af5aad00163a375d
Alan Franzoni <username@franzoni.eu>
2012-01-23 22:58:33 +0100

    Fix: test section

--- Library/Formula/mangle.rb
+++ Library/Formula/mangle.rb
@@ -26,11 +26,6 @@ EOS
   end
 
   def test
-    # This test will fail and we won't accept that! It's enough to just
-    # replace "false" with the main program this formula installs, but
-    # it'd be nice if you were more thorough. Test the test with
-    # `brew test mangle`. Remove this comment before submitting
-    # your pull request!
-    system "false"
+    system "python2.7 -c 'import mangle'"
   end
 end
